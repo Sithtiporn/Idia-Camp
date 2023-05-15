9f338e6db9493dded12ea8c6fa5ca7ad2c6989c5
Sithtiporn <122552150+Sithtiporn@users.noreply.github.com>
2023-05-15 15:49:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("ความจุของถังน้ำ: ");
+        double Vmax = double.Parse(Console.ReadLine());
+
+        Console.Write("ปริมาตรน้ำเฉลี่ยที่ผู้เข้าร่วมโครงการดื่มในแต่ละช่วงพัก: ");
+        double Vdrink = double.Parse(Console.ReadLine());
+
+        Console.Write("ปริมาตรน้ำที่เติมได้ในแต่ละรอบเติมน้ำ: ");
+        double Vfill = double.Parse(Console.ReadLine());
+
+        Console.Write("ระยะเวลาคั่นระหว่างช่วงพัก: ");
+        double tdrink = double.Parse(Console.ReadLine());
+
+        Console.Write("ระยะเวลาคั่นระหว่างรอบเติมน้ำ: ");
+        double tfill = double.Parse(Console.ReadLine());
+
+        Console.Write("ระยะเวลารวมของกิจกรรมตั้งแต่เริ่มจนจบวัน: ");
+        double tday = double.Parse(Console.ReadLine());
+
+        // คำนวณปริมาตรน้ำที่ต้องเติมในแต่ละวัน
+        double Vtotal = (Vdrink * (tday / (tdrink + tfill))) + Vfill * (tday / tfill);
+
+        // ตรวจสอบปริมาณน้ำที่เหลือ
+        if (Vtotal <= Vmax)
+        {
+            Console.WriteLine("Enough Water, {0} left", Vmax - Vtotal);
+        }
+        else if (Vtotal <= Vmax + Vfill)
+        {
+            Console.WriteLine("Overflow Water");
+        }
+        else
+        {
+            Console.WriteLine("Not Enough Water");
+        }
+
+        Console.ReadLine();
+    }
+}
